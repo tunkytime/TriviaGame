e1721a1ada8b85602bbe7943e7933d00a7ea1312
Alex <43651369+tunkytime@users.noreply.github.com>
2019-03-08 15:52:45 -0700

    functioning game, need to build in reset

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   
 	var timeLeft = 3;
 	var interval;
-	var correctCount;
-	var incorrectCount;
+	var correctCount = 0;
+	var incorrectCount = 0;
+	var unansweredCount = 0;
 	var questionNum = 0;
 	
 	var questionsArr = [
@@ -33,45 +34,115 @@ $(document).ready(function() {
 			dummyAnswers: ["dummy13", "dummy14", "dummy15"],
 		},
 	];
-
+	
 	function generateQuestion (array) {
-		var question = array[questionNum].question;
-		$("#question").text(question);
-		var answerDiv = $(`<div class="answer">${array[questionNum].answerCorrect}</div>`);
-		$("#answer-container").html(answerDiv);
-		for (var i = 0; i < array[questionNum].dummyAnswers.length; i++) {
-			var answerDiv = $(`<div class="answer">${array[questionNum].dummyAnswers[i]}</div>`);
-			$("#answer-container").append(answerDiv);
+		if (questionNum === (questionsArr.length - 1)) {
+			endGame();
+		} else {	
+			var question = array[questionNum].question;
+			$("#question").text(question);
 		};
 	};
+	
+	function generateAnswers(array) {
+			var correctAnswerDiv = $(`<div class="answer correct">${array[questionNum].answerCorrect}</div>`);
+			$("#answer-container").html(correctAnswerDiv);
+				for (var i = 0; i < array[questionNum].dummyAnswers.length; i++) {
+					var dummyAnswersDiv = $(`<div class="answer incorrect">${array[questionNum].dummyAnswers[i]}</div>`);
+					$("#answer-container").append(dummyAnswersDiv);
+				}	
+			
+			// On click events
+			$(".answer").on("click", function () {			
+				if( $(this).hasClass("correct")) {
+					correctCount++;
+					$(".answer").off("click");
+				} else {
+					incorrectCount++;
+					$(".answer").off("click");
+				}
+				showAnswer();
+				stop();
+				questionNum++;
+				setTimeout(resetTimer, 2000);
+				setTimeout(run, 2000);
+			});		
+	};
+	
+	function showAnswer() {
+		if (questionNum === questionsArr.length) {
+			endGame(); 
+		} else {
+		var newDiv = $(`<div id="show-answer"></div>`);
+		$("#show-answer").html(`The correct answer was ${questionsArr[questionNum].answerCorrect}`);
+	};
+	};
 		
+	// Starts countdown timer
 	function run() {
+		clearInterval(interval);
+		$("#show-time").text(timeLeft);
 		interval = setInterval(decrement, 1000);
 	};
-  
+	
+	// Decrements time by 1 second and ends after cycling through questionsArr
 	function decrement() {
 		timeLeft--;
 		$("#show-time").text(timeLeft);
 		if (timeLeft === 0) {
-			reset();
-			questionNum++;	
-			console.log(questionNum);
-			generateQuestion(questionsArr);
-		} 
-		if (questionNum === questionsArr.length) {
+			showAnswer();
 			stop();
-		}
+			questionNum++;
+			unansweredCount++;
+			setTimeout(resetTimer, 2000);
+			setTimeout(run, 2000);
+		} 
 	};
 	
-	function reset() {
-		timeLeft = 3;
+	// Rests timer after each question
+	function resetTimer() {
+		if (questionNum === questionsArr.length) {
+			endGame();
+		} else {
+			$("#show-answer").empty();
+			timeLeft = 3;
+			generateQuestion(questionsArr);
+			generateAnswers(questionsArr);
+		}
 	};
 	
 	function stop() {
 		clearInterval(interval);
-	}
+	};
 	
+	function endGame() {
+		stop();
+		$(".main-section").html
+		(`<h4>Game Summary</h4>
+		<p>Correct Answers: ${correctCount}</p>
+		<p>Incorrect Answers: ${incorrectCount}</p>
+		<p>Unanswered Questions: ${unansweredCount}</p>
+		<br>
+		<button id="reset-game">Play Again?</button>`);
+		
+		$("#reset-game").on("click", function () {
+			resetGame();
+		});
+	};
+	
+	// Resets the game
+	function resetGame() {
+		$(".main-section").html
+		(`<div id="gameplay">
+		<h4 class="mb-3">Time Remaining: <span id="show-time">3</span> Seconds</h4>
+		<strong><span id="question">Question goes here:</span></strong><br>
+		<div id="answer-container">
+		</div>
+		</div>`)
+	};
+	
+	// Starts the game 1st time
 	run();
-	generateQuestion(questionsArr);
-	
+	generateQuestion(questionsArr);	
+	generateAnswers(questionsArr);
 });
