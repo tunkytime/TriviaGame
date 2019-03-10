5a4f8a5d8e96e70b6aad30eb09d594be006a07ef
aschaaf <schaaf1091@gmail.com>
2019-03-09 23:37:40 -0700

    game complete, need to write instructions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,51 @@
 $(document).ready(function () {
 
 	// VARIABLES //
-	var timeLeft = 3;
+	var startTime = 20;
+	var timeLeft = startTime;
 	var interval;
 	var correctCount = 0;
 	var incorrectCount = 0;
 	var unansweredCount = 0;
 	var questionNum = 0;
-	var question = "";
+	var timeout = 4000;
 
 	var questionsArr = [{
-			question: "Question #1",
-			correct: "correct #1",
-			choices: ["correct #1", "dummy1", "dummy2", "dummy3"],
+			question: `Complete this line by the Joker: "If you're good at something, never do it for ________."`,
+			correct: "Free",
+			choices: ["Free", "Fun", "Yourself", "Money"],
+			mp3: "assets/sounds/free.mp3",
 		},
 		{
-			question: "Question #2",
-			correct: "correct #2",
-			choices: ["correct #2", "dummy4", "dummy5", "dummy6"],
+			question: `Who said this first? "You either die a hero, or you live long enough to see yourself become the villain."`,
+			correct: "Harvey Dent",
+			choices: ["Harvey Dent", "Batman", "The Joker", "Commissioner Gordon"],
+			mp3: "assets/sounds/hero.mp3",
 		},
 		{
-			question: "Question #3",
-			correct: "correct #3",
-			choices: ["correct #3", "dummy7", "dummy8", "dummy9"],
+			question: `Complete the following line by Harvey Dent: "It's not about what I want - it's about what's ________!"`,
+			correct: "Fair",
+			choices: ["Fair", "Right", "Necessary", "Legal"],
+			mp3: "assets/sounds/fair.mp3",
+
 		},
 		{
-			question: "Question #4",
-			correct: "correct #4",
-			choices: ["correct #4", "dummy10", "dummy11", "dummy12"],
+			question: `What was The Joker's first line in the movie?`,
+			correct: "And I thought my jokes were bad.",
+			choices: ["And I thought my jokes were bad.", "Nobody panics when things go according to plan.", "Good evening ladies and gentlemen!", "Why so serious?"],
+			mp3: "assets/sounds/jokes.mp3",
 		},
 		{
-			question: "Question 5?",
-			correct: "correct #5",
-			choices: ["correct #5", "dummy13", "dummy14", "dummy15"],
+			question: `What kind of car does Bruce Wayne drive?`,
+			correct: "Lamborghini",
+			choices: ["Lamborghini", "Bugatti", "Rolls-Royce", "Ferrari"],
+			mp3: "assets/sounds/subtle.mp3",
+		},
+		{
+			question: `What was The Joker's signature phrase?`,
+			correct: "Why so serious?",
+			choices: ["Why so serious?", "You're not playing the odds, friend", "It's not about money", "How 'bout a magic trick?"],
+			mp3: "assets/sounds/serious.mp3",
 		},
 	];
 
@@ -56,26 +69,9 @@ $(document).ready(function () {
 		};
 	};
 
-	// // Generate list of 20 questions and answers from API | Category = Science: Computers
-	// var queryURL = "https://opentdb.com/api.php?amount=20&category=18&difficulty=easy&type=multiple";
-	// $.ajax({
-	// 	url: queryURL,
-	// 	method: "GET"
-	// }).then(function (response) {
-	// 	newQuestion(response);
-	// });
-
-	// var newQuestion = function (data) {
-	// 	console.log(data.results[questionNum].question)
-	// 	console.log(`Correct Answer: ${data.results[questionNum].correct_answer}`)
-	// 	console.log(data.results[questionNum].incorrect_answers[0]);
-	// 	console.log(data.results[questionNum].incorrect_answers[1]);
-	// 	console.log(data.results[questionNum].incorrect_answers[2]);
-	// };
-	// // end of database question functions
-
 	// Display the first question
 	function generateQuestion(array) {
+		hideAnswer();
 		var question = array[questionNum].question;
 		$("#question").text(question);
 	};
@@ -89,10 +85,10 @@ $(document).ready(function () {
 
 		for (var i = 0; i < array[questionNum].choices.length; i++) {
 			if (i !== indexCorrect) {
-				var choicesDiv = $(`<div class="choice">${array[questionNum].choices[i]}</div>`);
+				var choicesDiv = $(`<div class="choice rounded">${array[questionNum].choices[i]}</div>`);
 				$("#answer-container").append(choicesDiv);
 			} else {
-				var correctDiv = $(`<div class="choice correct">${correctAnswer}</div>`);
+				var correctDiv = $(`<div class="choice correct rounded">${correctAnswer}</div>`);
 				$("#answer-container").append(correctDiv);
 			};
 		};
@@ -109,19 +105,23 @@ $(document).ready(function () {
 			showAnswer();
 			stop();
 			questionNum++;
-
-			newQuestion = function (data) {
-				console.log(data.results[questionNum].question)
-			};
-
-			setTimeout(resetTimer, 2000);
-			setTimeout(run, 2000);
+			setTimeout(resetTimer, timeout);
+			setTimeout(run, timeout);
 		});
 	};
 
 	// Show answer
 	function showAnswer() {
-		$("#show-answer").html(`The correct answer was ${questionsArr[questionNum].correct}`);
+		$("#show-answer").css("display", "block");
+		$("#show-answer").html(`The correct answer was "${questionsArr[questionNum].correct}"`);
+		var playQuote = document.createElement("audio");
+		playQuote.setAttribute("src", " " + questionsArr[questionNum].mp3 + "");
+		playQuote.play();
+	};
+
+	// Hide answer
+	function hideAnswer() {
+		$("#show-answer").css("display", "none");
 	};
 
 	// Starts countdown timer
@@ -140,8 +140,8 @@ $(document).ready(function () {
 			stop();
 			questionNum++;
 			unansweredCount++;
-			setTimeout(resetTimer, 2000);
-			setTimeout(run, 2000);
+			setTimeout(resetTimer, timeout);
+			setTimeout(run, timeout);
 		};
 	};
 
@@ -151,7 +151,7 @@ $(document).ready(function () {
 			endGame();
 		} else {
 			$("#show-answer").empty();
-			timeLeft = 3;
+			timeLeft = startTime;
 			generateQuestion(questionsArr);
 			generateAnswers(questionsArr);
 		};
@@ -163,27 +163,27 @@ $(document).ready(function () {
 
 	function endGame() {
 		stop();
-		$(".main-section").html(`<h4>Game Summary</h4>
+		$(".main-section").html(`<h4><strong>Game Summary</strong></h4>
+			<hr>
 			<p>Correct Answers: ${correctCount}</p>
 			<p>Incorrect Answers: ${incorrectCount}</p>
 			<p>Unanswered Questions: ${unansweredCount}</p>
-			<br>
-			<button id="reset-game">Play Again?</button>`);
+			<p>Your Score: ${correctCount/(correctCount+incorrectCount+unansweredCount)*100}%</p>
+			<button id="reset-game" class="btn btn-block text-white">Play Again?</button>`);
 
 		$("#reset-game").on("click", function () {
-			timeLeft = 3;
+			timeLeft = startTime;
 			interval;
 			correctCount = 0;
 			incorrectCount = 0;
 			unansweredCount = 0;
 			questionNum = 0;
-			$(".main-section").html(`
-			
-			<div id="gameplay">
-				<h4 class="mb-3">Time Remaining: <span id="show-time">3</span> Seconds</h4>
-				<strong><span id="question">Question goes here:</span></strong>
-				<div id="answer-container"></div>
-				<div id="show-answer"></div>
+			$("#gameplay").html(`<div class="main-section">
+			<h4 class="mb-3">Time Remaining: <strong><span id="show-time">3</span> Seconds</strong></h4>
+			<strong><span id="question"></span></strong>
+			<br>
+			<div id="answer-container"></div>
+			<div id="show-answer"></div>
 			</div>`)
 			shuffleAnswers();
 			setTimeout(generateQuestion(questionsArr), 1000);
@@ -192,8 +192,12 @@ $(document).ready(function () {
 	};
 
 	// START THE GAME //
-	shuffleAnswers();
-	run();
-	generateQuestion(questionsArr);
-	generateAnswers(questionsArr);
+	$("#start-game").on("click", function () {
+		$("#gameplay").css("display", "block");
+		$("#start-game").empty();
+		shuffleAnswers();
+		run();
+		generateQuestion(questionsArr);
+		generateAnswers(questionsArr);
+	});
 });
